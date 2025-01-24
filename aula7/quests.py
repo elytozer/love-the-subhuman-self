71934f738cc3b04264a21beca64b4619ead54bb5
elyton <elyton.sousa@academico.ifpb.edu.br>
2025-01-24 17:42:01 +0000

    24/01/25

---
+++ aula7/quests.py
@@ -0,0 +1,6 @@
+#Escreva uma função chamada saudacao que aceita um nome como argumento e imprime "Olá, [nome]!".
+def saudacao(nome):
+    print(f'Olá, {nome}!')
+
+#Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
+def dobro
