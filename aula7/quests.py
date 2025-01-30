deae515516199e83939f5a6460e060ea9e5aba94
elyton <elyton.sousa@academico.ifpb.edu.br>
2025-01-30 17:36:17 +0000

    30/01/25

--- aula7/quests.py
+++ aula7/quests.py
@@ -1,6 +1,17 @@
 #Escreva uma função chamada saudacao que aceita um nome como argumento e imprime "Olá, [nome]!".
 def saudacao(nome):
+    nome = input("digite seu nome:")
     print(f'Olá, {nome}!')
 
 #Crie uma função dobro que aceita um número como argumento e retorna o dobro desse número.
-def dobro
+def dobro(dobro):
+    num = input("")
+    dobro = num * 2
+    print = ({dobro})
+
+#Crie uma função chamada saudacao_personalizada que aceita um nome e um idioma como argumentos. O idioma é opcional e padrão para "inglês". A função deve retornar uma saudação no idioma especificado.
+def saudacao_personalizada(nome, idioma = "inglês"):
+    if idioma == "inglês"
+        return f"hello {nome}"
+    elif idioma == "español"
+        return f"hola {nome}"   
